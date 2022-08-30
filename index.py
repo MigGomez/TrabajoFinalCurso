161aa78bd0aa7048ba9fc918868475fce47a17c5
Miguelchz <ch12043@ues.edu.sv>
2022-08-30 10:59:55 -0600

    agregada la interfaz web

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+from re import sub
+import subprocess
 import os
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
@@ -6,7 +8,7 @@ from werkzeug.datastructures import  FileStorage
 
 
 app= Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './Archivos excel'
+app.config['UPLOAD_FOLDER'] = './Datos Ingresos'
 ALLOWED_EXTENSIONS = set(['.xls'])
 
 @app.route('/')
@@ -23,18 +25,21 @@ def uploader():
         return render_template("index.html", msg="Archivos cargados correctamente")
 
     return render_template("index.html", msg="Seleccionar archivos")
-
-@app.route("/obtener", methods=["GET"])
+5
+#este metodo es para el web scraping pero por el momento lo dejaremos comentado si no les va a alterar los
+#datos obtenidos este dia y no les van a concordar con los del ing.
+''' @app.route("/obtener", methods=["GET"])
 def obtener():
-
-
     if request.method == 'GET':
-        script='./Script/cinemas.py'
-        exec(open(script).read())
+        subprocess.call(['python ','Cinepolis Horarios.py'])
     return render_template("index.html", msg2="Datos obtenidos correctamente")
-
+ '''
+@app.route("/depurar", methods=["GET"])
+def depurar():
+    if request.method == 'GET':
+        subprocess.call(['python ','Limpiar Datos Ingresos.py'])
+    return render_template("index.html", msg3="Datos listos y depurados")
     
-
 if __name__ == '__main__':
     app.run(debug=True)
     
